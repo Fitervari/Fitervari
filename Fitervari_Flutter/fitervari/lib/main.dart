4cf771d9b3900527780f2c3f8da808d1da61dfc7
TheSteve1 <68355734+TheSteve1@users.noreply.github.com>
2021-12-19 01:32:01 +0100

    working demo for meeting

--- Fitervari_Flutter/fitervari/lib/main.dart
+++ Fitervari_Flutter/fitervari/lib/main.dart
@@ -1,3 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:wearable_communicator/wearable_communicator.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController _controller;
+  String value = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+
+    WearableListener.listenForMessage((msg) {
+      print(msg);
+    });
+    WearableListener.listenForDataLayer((msg) {
+      print(msg);
+    });
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                    border: InputBorder.none, labelText: 'Enter some text'),
+                onChanged: (String val) async {
+                  setState(() {
+                    value = val;
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text('Send message to wearable'),
+                onPressed: () {
+                  primaryFocus.unfocus(disposition: UnfocusDisposition.scope);
+                  WearableCommunicator.sendMessage({"text": value});
+                },
+              ),
+              RaisedButton(
+                child: Text('set data on wearable'),
+                onPressed: () {
+                  primaryFocus.unfocus(disposition: UnfocusDisposition.scope);
+                  WearableCommunicator.setData("message", {
+                    "text": value != ""
+                        ? value
+                        : "test", // ensure we have at least empty string
+                    "integerValue": 1,
+                    "intList": [1, 2, 3],
+                    "stringList": ["one", "two", "three"],
+                    "floatList": [1.0, 2.4, 3.6],
+                    "longList": []
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 /*import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:client_server_lan/client_server_lan.dart';
@@ -287,7 +374,7 @@ class _HomePageState extends State<HomePage> {
       },
     );
   }
-}*/
+}
 
 import 'package:fitervari/screens/plan.dart';
 import 'package:flutter/material.dart';
@@ -333,3 +420,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
